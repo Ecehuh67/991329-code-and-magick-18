0559d74dffd00c0cb95b2862604d23a5c35c0a57
Гендольф <ecehuh67@gmail.com>
2019-09-19 14:41:36 +0300

    Another version

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var AMOUNT_OF_WIZZARD = 4; // Количество магов в блоке выбора
+var AMOUNT_OF_WIZARD = 4; // Количество магов в блоке выбора
 var WIZZARD_NAMES = ['Иван', 'Хуан', 'Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон']; // Имена Магов
 var WIZZARD_SECOND_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг']; // Фамилии магов
 var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)']; // Цвет мантии
@@ -11,8 +11,6 @@ var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green']; // Цвет гла
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
-document.querySelector('.setup-similar').classList.remove('hidden');
-
 // Находим ссылки на блок маговв который будет вставлять магов из шаблона
 var similatListElement = document.querySelector('.setup-similar-list');
 
@@ -21,26 +19,47 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
   .content.
 querySelector('.setup-similar-item');
 
-// Генерация случайного имени и фамилии
-var getRandomNumber = function (data) {
+// Генерация случайного числа
+var getRandomData = function (data) {
   return Math.round(Math.random() * (data.length - 1));
 };
 
-// Создание случайного персонажа и вставка его в блок
-for (var i = 0; i < AMOUNT_OF_WIZZARD; i++) {
+// Шаблон массива с магами
+var getWizards = function (amount) {
+  var wizardsData = [];
+  for (var j = 0; j < amount; j++) {
+    var characteristics = {
+      name: WIZZARD_NAMES[getRandomData(WIZZARD_NAMES)] + ' ' + WIZZARD_SECOND_NAMES[getRandomData(WIZZARD_SECOND_NAMES)],
+      coatColor: COAT_COLOR[getRandomData(COAT_COLOR)],
+      eyesColor: EYES_COLOR[getRandomData(EYES_COLOR)]
+    };
+    wizardsData[j] = characteristics;
+  }
+  return wizardsData;
+};
 
-  // Копируем из шаблона все элементы
+var wizards = getWizards(AMOUNT_OF_WIZARD);
+
+// Создаем мага согласно шаблона
+var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
-  // Подстановка сгенерированных данных в блок персонажа
-  wizardElement.querySelector('.setup-similar-label').textContent = WIZZARD_NAMES[getRandomNumber(WIZZARD_NAMES)] + ' ' + WIZZARD_SECOND_NAMES[getRandomNumber(WIZZARD_SECOND_NAMES)];
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
 
-  // Подстановка случайного цвета мантии
-  wizardElement.querySelector('.wizard-coat').setAttribute('fill', COAT_COLOR[getRandomNumber(COAT_COLOR)]);
+  return wizardElement;
+};
 
-  // Подстановка случайного цвета глаз
-  wizardElement.querySelector('.wizard-eyes').setAttribute('fill', EYES_COLOR[getRandomNumber(EYES_COLOR)]);
+// Создаем буфер куда будем временно копировать магов
+var fragment = document.createDocumentFragment();
 
-  // Вставляет скопированные элементы в блок похожих магов
-  similatListElement.appendChild(wizardElement);
+// Копируем магов в буфер
+for (var i = 0; i < wizards.length; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
 }
+
+// Переносим содержимое буфера в разметку
+similatListElement.appendChild(fragment);
+
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
