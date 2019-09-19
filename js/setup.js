79912ef426c4d230b1e1ef706b19391a4a243fc6
Гендольф <ecehuh67@gmail.com>
2019-09-19 11:39:15 +0300

    training project

---
+++ js/setup.js
@@ -0,0 +1,45 @@
+'use strict'
+var AMOUNT_OF_WIZZARD = 4; //Количество магов в блоке выбора
+var WIZZARD_NAMES = ['Иван', 'Хуан', 'Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон']; // Имена Магов
+var WIZZARD_SECOND_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг']; // Фамилии магов
+var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)']; // Цвет мантии
+var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green']; // Цвет глаз
+
+
+// Показываем диалоговое окно выбора мага
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove("hidden");
+
+document.querySelector('.setup-similar').classList.remove('hidden');
+
+//Находим ссылки на блок маговв который будет вставлять магов из шаблона
+var similatListElement = document.querySelector('.setup-similar-list');
+
+//Находим шаблон мага
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+  .content.
+querySelector('.setup-similar-item');
+
+//Генерация случайного имени и фамилии
+var getRandomNumber = function(data) {
+  return  Math.round(Math.random() * (data.length-1));
+}
+
+//Создание случайного персонажа и вставка его в блок
+for (var i = 0; i < AMOUNT_OF_WIZZARD; i++) {
+
+//Копируем из шаблона все элементы
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+//Подстановка сгенерированных данных в блок персонажа
+  wizardElement.querySelector('.setup-similar-label').textContent = WIZZARD_NAMES[getRandomNumber(WIZZARD_NAMES)] + ' ' + WIZZARD_SECOND_NAMES[getRandomNumber(WIZZARD_SECOND_NAMES)];
+
+//Подстановка случайного цвета мантии
+  wizardElement.querySelector('.wizard-coat').setAttribute('fill', COAT_COLOR[getRandomNumber(COAT_COLOR)]);
+
+//Подстановка случайного цвета глаз
+  wizardElement.querySelector('.wizard-eyes').setAttribute('fill', EYES_COLOR[getRandomNumber(EYES_COLOR)]);
+
+//Вставляет скопированные элементы в блок похожих магов
+  similatListElement.appendChild(wizardElement);
+}
--- js/stat.js
+++ js/stat.js
@@ -13,11 +13,13 @@ var COLUMN_WIDTH = 40;
 var BAR_HEIGHT = 150;
 var SPACE_BETWEEN = 50;
 
+//Отрисовка всплывающего окна с тенью
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
+//Находим масксимальное значение очков среди игроков
 var getMaxElement = function (score) {
   var maxElement = score[0];
   for (var i = 0; i < score.length; i++) {
@@ -28,6 +30,8 @@ var getMaxElement = function (score) {
   return Math.round(maxElement);
 };
 
+
+
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
@@ -35,6 +39,7 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
 
+//Находим имя игрока с наибольшим количеством очков
   for (var k = 0; k < times.length; k++) {
     var numberOfWinner = k;
     if (Math.round(times[k]) === getMaxElement(times)) {
@@ -42,6 +47,7 @@ window.renderStatistics = function (ctx, names, times) {
     }
   }
 
+//Проверка кто выиграл и вывод сообщения
   if (names[numberOfWinner] === 'Вы') {
     ctx.fillText('Ура вы победили!', MESSAGE_X, MESSAGE_Y);
   } else {
@@ -60,6 +66,7 @@ window.renderStatistics = function (ctx, names, times) {
     ctx.fillStyle = '#000';
     ctx.fillText(names[i], columnX, columnY);
 
+//Задаем цвет столбцов и рисуем их
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
@@ -67,6 +74,7 @@ window.renderStatistics = function (ctx, names, times) {
     }
     ctx.fillRect(columnX, columnY - FONT - GAP - columnHeight, COLUMN_WIDTH, columnHeight);
 
+//Указываем счет игрока сверху столбца
     ctx.fillStyle = '#000';
     ctx.fillText(Math.round(times[i]), columnX, columnY - FONT - GAP - columnHeight - GAP);
   }
